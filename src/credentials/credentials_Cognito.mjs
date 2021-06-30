9b7f36efe4fe9b362ad89a8348939957ba640e51
Seth <sethborne@gmail.com>
2021-06-29 20:30:07 -0700

    adds base extraction for credentials

---
+++ src/credentials/credentials_Cognito.mjs
@@ -0,0 +1,3 @@
+import Creds from './secrets/cognitoSecrets.json';
+
+export const CREDENTIALS = Creds;
---
+++ src/credentials/credentials_s3.mjs
@@ -0,0 +1,3 @@
+import secrets_s3Upload from './secrets/s3UploadSecrets.json';
+
+export const creds_s3Uploads = secrets_s3Upload;
